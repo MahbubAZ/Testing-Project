99447df0031bc6ef2224cea7bb7ed3cbc43e9191
Mohammad Mahbubur Rahman <mohammadmahbuburrahman@Mohammads-MacBook-Air.local>
2020-11-29 02:26:21 +0600

    Added a new method called greeting in Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,10 @@
 public class Hello{
     public static void main(String[] args){
         System.out.println("VS Code is awesome...");
+        greeting("Mahbub");
+    }
+
+    public static void greeting(String name){
+        System.out.println("Welcome, "+ name +"!!");
     }
 }
